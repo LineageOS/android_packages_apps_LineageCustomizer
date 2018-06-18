7699c8524a130bb4901ebe3fd9dca40012a5cf2b
Aaron Kling <webgeek1234@gmail.com>
2018-06-18 13:49:01 -0500

    Initial bringup to tvlauncher config style
    Most configurations can be overridden per device by overlaying
    res/raw/configuration.xml
    
    Change-Id: Ibc2917ea2a63941ff94e99a670ef5628751c4a29

---
+++ src/org/lineageos/tvcustomizer/StubReceiver.java
@@ -0,0 +1,12 @@
+package org.lineageos.tvcustomizer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class StubReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        Log.d("StubReceiver", "LineageCustomize.StubReceiver called");
+    }
+}
