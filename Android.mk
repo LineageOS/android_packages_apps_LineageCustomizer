902123e64d4f365749d692baa23909f216cebc82
Aaron Kling <webgeek1234@gmail.com>
2018-08-16 16:47:02 -0500

    Define LOCAL_SDK_VERSION
    Change-Id: I9751f2d33c2158d170a24cae35877216de024559

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := LineageCustomizer
+LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
