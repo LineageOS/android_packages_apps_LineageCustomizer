d00a1e17c04629a14ab324b30c2c646828a5505b
Sean Hoyt <seanhoyt963@gmail.com>
2018-09-04 19:52:23 +0200

    Correct package name
    Change-Id: I0c9c1bf46db7d621918e9ec75baea1dc5ddeb1ca

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/priv-app
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := LineageCustomize
+LOCAL_PACKAGE_NAME := LineageCustomizer
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SDK_VERSION := current
